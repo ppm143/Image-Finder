c2ea8e1713f9d118e0f13baf8c1f3232d7e722a1
ppm_143 <parthpatel.py@gmail.com>
2020-09-23 15:51:34 +0530

    first commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar'
+
+const Navbar = () => {
+    return (
+        <AppBar title="Image Finder" />
+    )
+}
+
+export default Navbar;
